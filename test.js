abcaafcd8e6d3e1401937994fcfab8f3301e4c43
Daniel Röhers Moura <rohersmoura@gmail.com>
2016-07-02 21:48:57 -0300

    script weka

--- test.js
+++ test.js
@@ -1,14 +1,49 @@
 'use strict';
 
-const brain = require('brain');
+require('./config/mongoose');
 
+const AnswerModel = require('./app/models/answer');
+const CourseModel = require('./app/models/course');
+const QuestionModel = require('./app/models/question');
+
+const _ = require('lodash');
+const brain = require('brain');
 const net = new brain.NeuralNetwork();
 
-net.train([
-    { input: { 'test-1': 'hahaha' }, output: { 'si': 1 } },
-    { input: { 'test-1': 'hihihi' }, output: { 'design': 1 } },
-]);
+const _train = answers => {
+    let inputs = [];
+    answers.forEach(answer => {
+        let data = { input : {}, output: {} };
+        answer.input.forEach(input => data['input'][input.question] = input.option );
+        data['output'][answer.output.name] = 1;
+        inputs.push(data);
+    });
+    net.train(inputs, {
+        errorThresh: 0.005,  // error threshold to reach
+        iterations: 20000,   // maximum training iterations
+        log: true,           // console.log() progress periodically
+        logPeriod: 10,       // number of iterations between logging
+        learningRate: 0.3    // learning rate
+    });
+};
 
-let output = net.run({ 'test-1': 'hahaha' });
+AnswerModel
+    .find({})
+    .populate('output')
+    .then(answers => {
+        let select = _.take(_.shuffle(answers), 15);
 
-console.log("output: ", output);
+        _train(answers);
+
+        select.forEach(answer => {
+            console.log("course: ", answer.output.name);
+
+            let data = {};
+            answer.input.forEach(input => data[input.question] = input.option );
+
+            let result = [];
+            _.forEach(net.run(data), (value, course) => result.push({ course: course, value: value }) );
+            console.log(_.orderBy(result, 'value', 'desc'));
+        })
+    })
+    .catch(err => console.log('ERR: ', err));
---
+++ weka.js
@@ -0,0 +1,82 @@
+'use strict';
+
+require('./config/mongoose');
+
+const AnswerModel = require('./app/models/answer');
+const CourseModel = require('./app/models/course');
+const QuestionModel = require('./app/models/question');
+
+const async = require('async');
+const fs = require('fs');
+const _ = require('lodash');
+const brain = require('brain');
+const slug = require('slug');
+const net = new brain.NeuralNetwork();
+
+const _filename = 'data.arff';
+
+const _deleteFile = cb => {
+    if (!fs.existsSync(_filename)) return cb();
+    fs.unlink('treinamento.arff', cb);
+};
+
+const _append = (text, cb) => {
+    fs.appendFile('treinamento.arff', `${text}\n`, cb);
+};
+
+const _err = err => console.log('ERR: ', err);
+
+const _addRelation = cb => _append('@RELATION faccat\n', cb);
+
+const _addAttributes = cb => {
+    QuestionModel
+        .find({})
+        .then(questions => {
+            questions.forEach(question => {
+                _append(`@ATTRIBUTE ${question._id} NUMERIC`);
+            });
+            cb();
+        })
+        .catch(cb);
+};
+
+const _addClass = cb => {
+    CourseModel
+        .find({})
+        .then(courses => {
+            _append(`\n@ATTRIBUTE class {${ courses.map(course => slug(course.name)).join() }}\n`, cb);
+        })
+        .catch(cb)
+};
+
+const _addData = cb => {
+    AnswerModel
+        .find({})
+        .populate('output')
+        .then(answers => {
+            _append('@DATA')
+            answers.forEach(answer => {
+                var values = [];
+                answer.input.forEach(input => values.push(input.option) );
+                values.push(slug(answer.output.name));
+                _append(values.join());
+            });
+            cb();
+        })
+        .catch(cb);
+};
+
+const _init = () => {
+    async.series([
+        _deleteFile,
+        _addRelation,
+        _addAttributes,
+        _addClass,
+        _addData
+    ], (err) => {
+        if (err) console.log('ERR: ', err);
+        console.log('DONE');
+    });
+};
+
+_init();
